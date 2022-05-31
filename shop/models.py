608323ab1722c9e7df01114ec3c0214c1cc028e2
Hossein.Gholami <h.reza73327@gmail.com>
2022-05-31 17:54:24 +0430

    add image

--- shop/models.py
+++ shop/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     title = models.CharField(max_length=50)
     desc = models.CharField(max_length=120)
     price = models.IntegerField(default=10, null=False)
+    image = models.CharField(max_length=500, default="https://cdn3.iconfinder.com/data/icons/retail-13/100/location-pin-512.png")
 
     def __str__(self):
         return self.title
